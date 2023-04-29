98b37bd606c490361be68be3c10ba550cfd6a7d2
Jan Kotas <jan.kotas@eman.cz>
2023-04-30 00:03:27 +0200

    ♻️ Update UIEvent

--- core/src/main/kotlin/com/plcoding/core/util/UIEvent.kt
+++ core/src/main/kotlin/com/plcoding/core/util/UIEvent.kt
@@ -2,5 +2,8 @@ package com.plcoding.core.util
 
 sealed class UIEvent {
     data class Navigate(val route: String) : UIEvent()
+
     object NavigateUp : UIEvent()
+
+    data class ShowSnackBar(val message: UiText) : UIEvent()
 }
