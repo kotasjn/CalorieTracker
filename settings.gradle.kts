034f72e54da006a757d822f60e5dbb866a754aad
Jan Kotas <jan.kotas@eman.cz>
2023-04-29 13:41:33 +0200

    ♻️ Migrate settings.gradle from Groovy to Kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CaloryTracker"
+
+include(":app")
+
+include(":core")
+
+include(":coreui")
+
+include(
+    ":onboarding:presentation",
+    ":onboarding:domain",
+)
+
+include(
+    ":tracker:data",
+    ":tracker:domain",
+    ":tracker:presentation",
+)
