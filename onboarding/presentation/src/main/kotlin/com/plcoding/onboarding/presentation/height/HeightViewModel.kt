6d72dfe22a0e3f5c4b38eaed9639104098e42336
Jan Kotas <jan.kotas@eman.cz>
2023-05-01 11:38:01 +0200

    💄 Add height screen and viewmodel

---
+++ onboarding/presentation/src/main/kotlin/com/plcoding/onboarding/presentation/height/HeightViewModel.kt
@@ -0,0 +1,49 @@
+package com.plcoding.onboarding.presentation.height
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.core.R
+import com.plcoding.core.domain.preferences.Preferences
+import com.plcoding.core.domain.usecase.FilterOutDigits
+import com.plcoding.core.navigation.Route
+import com.plcoding.core.util.UIEvent
+import com.plcoding.core.util.UIText
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HeightViewModel @Inject constructor(
+    private val preferences: Preferences,
+    private val filterOutDigits: FilterOutDigits,
+) : ViewModel() {
+    var height by mutableStateOf("180")
+        private set
+
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onHeightEnter(height: String) {
+        if (height.length <= 3) {
+            this.height = filterOutDigits(height)
+        }
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            val heightNumber = height.toIntOrNull() ?: kotlin.run {
+                _uiEvent.send(
+                    UIEvent.ShowSnackBar(UIText.StringResource(R.string.error_height_cant_be_empty)),
+                )
+                return@launch
+            }
+            preferences.saveHeight(heightNumber)
+            _uiEvent.send(UIEvent.Navigate(Route.WEIGHT))
+        }
+    }
+}
