4d61d55d8f4034ae9a4f1965653edc09be6d4818
Jan Kotas <jan.kotas@eman.cz>
2023-05-01 23:04:28 +0200

    💄 Add nutrient goal screen and viewmodel

---
+++ onboarding/presentation/src/main/kotlin/com/plcoding/onboarding/presentation/nutrientGoal/NutrientGoalViewModel.kt
@@ -0,0 +1,77 @@
+package com.plcoding.onboarding.presentation.nutrientGoal
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.core.domain.preferences.Preferences
+import com.plcoding.core.domain.usecase.FilterOutDigits
+import com.plcoding.core.navigation.Route
+import com.plcoding.core.util.UIEvent
+import com.plcoding.onboarding.domain.usecase.ValidateNutrients
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NutrientGoalViewModel @Inject constructor(
+    private val preferences: Preferences,
+    private val filterOutDigits: FilterOutDigits,
+    private val validateNutrients: ValidateNutrients,
+) : ViewModel() {
+
+    var state by mutableStateOf(NutrientGoalState())
+        private set
+
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onEvent(event: NutrientGoalEvent) {
+        when (event) {
+            is NutrientGoalEvent.OnCarbRatioEnter -> {
+                state = state.copy(
+                    carbsRatio = filterOutDigits(event.ratio),
+                )
+            }
+
+            is NutrientGoalEvent.OnFatRatioEnter -> {
+                state = state.copy(
+                    fatRatio = filterOutDigits(event.ratio),
+                )
+            }
+
+            is NutrientGoalEvent.OnProteinRatioEnter -> {
+                state = state.copy(
+                    proteinRatio = filterOutDigits(event.ratio),
+                )
+            }
+
+            NutrientGoalEvent.OnNextClick -> {
+                val result = validateNutrients(
+                    carbsRationText = state.carbsRatio,
+                    proteinRatioText = state.proteinRatio,
+                    fatRatioText = state.fatRatio,
+                )
+                when (result) {
+                    is ValidateNutrients.Result.Success -> {
+                        preferences.saveCarbRatio(result.carbsRation)
+                        preferences.saveProteinRatio(result.proteinRatio)
+                        preferences.saveFatRatio(result.fatRatio)
+                        viewModelScope.launch {
+                            _uiEvent.send(UIEvent.Navigate(Route.TRACKER_OVERVIEW))
+                        }
+                    }
+
+                    is ValidateNutrients.Result.Error -> {
+                        viewModelScope.launch {
+                            _uiEvent.send(UIEvent.ShowSnackBar(result.message))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
