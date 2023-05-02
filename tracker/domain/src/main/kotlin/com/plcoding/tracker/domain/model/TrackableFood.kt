ed8938aba0035f0421ac486fb5e77b38da73144e
Jan Kotas <jan.kotas@eman.cz>
2023-05-03 00:00:28 +0200

    ✨ Add domain models

---
+++ tracker/domain/src/main/kotlin/com/plcoding/tracker/domain/model/TrackableFood.kt
@@ -0,0 +1,10 @@
+package com.plcoding.tracker.domain.model
+
+data class TrackableFood(
+    val name: String,
+    val imageUrl: String?,
+    val caloriesPer100g: Int,
+    val carbsPer100g: Int,
+    val proteinsPer100g: Int,
+    val fatPer100g: Int,
+)
