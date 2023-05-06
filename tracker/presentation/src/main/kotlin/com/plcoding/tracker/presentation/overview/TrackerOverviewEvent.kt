3443c9d80318bc524b8aec562dd683bef78a338b
Jan Kotas <jan.kotas@eman.cz>
2023-05-06 18:36:42 +0200

    ✨ Add tracker event and state classes

---
+++ tracker/presentation/src/main/kotlin/com/plcoding/tracker/presentation/overview/TrackerOverviewEvent.kt
@@ -0,0 +1,11 @@
+package com.plcoding.tracker.presentation.overview
+
+import com.plcoding.tracker.domain.model.TrackedFood
+
+sealed class TrackerOverviewEvent {
+    object OnNextDayClick : TrackerOverviewEvent()
+    object OnPreviousDayClick : TrackerOverviewEvent()
+    data class OnToggleMealClick(val meal: Meal) : TrackerOverviewEvent()
+    data class OnDeleteTrackedFoodClick(val trackedFood: TrackedFood) : TrackerOverviewEvent()
+    data class OnAddFoodClick(val meal: Meal) : TrackerOverviewEvent()
+}
