7ea498fac09c0fbb50e1eb04cb1545367e7db256
Jan Kotas <jan.kotas@eman.cz>
2023-06-09 15:55:29 +0200

    ♻️ Modify test settings to distinguish components from screens

--- screenshots/src/test/kotlin/com/plcoding/screenshots/PaparazziShowkaseTest.kt
+++ screenshots/src/test/kotlin/com/plcoding/screenshots/PaparazziShowkaseTest.kt
@@ -33,55 +33,99 @@ class ComponentPreview(
 }
 
 @RunWith(TestParameterInjector::class)
-class PaparazziShowkaseTests {
+class ComponentTests {
 
-    object PreviewProvider : TestParameter.TestParameterValuesProvider {
+    object ComponentPreviewProvider : TestParameter.TestParameterValuesProvider {
         override fun provideValues(): List<ComponentPreview> =
-            Showkase.getMetadata().componentList.map(::ComponentPreview)
+            Showkase.getMetadata().componentList.filter {
+                it.group == "Components"
+            }.map(::ComponentPreview)
     }
 
     @get:Rule
-    val paparazzi = Paparazzi(
+    val paparazziComponent = Paparazzi(
         maxPercentDifference = 0.0,
         showSystemUi = false,
         renderingMode = SessionParams.RenderingMode.SHRINK,
     )
 
     @Test
-    fun preview_tests(
-        @TestParameter(valuesProvider = PreviewProvider::class) componentPreview: ComponentPreview,
+    fun component_tests(
+        @TestParameter(valuesProvider = ComponentPreviewProvider::class) componentPreview: ComponentPreview,
         @TestParameter(value = ["1", "2"]) fontScale: Float,
         @TestParameter(value = ["light", "dark"]) theme: String,
     ) {
-        paparazzi.snapshot {
-            val lifecycleOwner = LocalLifecycleOwner.current
-            val configuration = if (theme == "dark") {
-                Configuration(LocalConfiguration.current).apply {
-                    uiMode = Configuration.UI_MODE_NIGHT_YES
-                }
-            } else {
-                LocalConfiguration.current
+        paparazziComponent.snapshot {
+            compositionProvider(fontScale = fontScale, theme = theme) {
+                componentPreview.content()
             }
+        }
+    }
+}
 
-            CompositionLocalProvider(
-                LocalInspectionMode provides true,
-                LocalDensity provides Density(
-                    density = LocalDensity.current.density,
-                    fontScale = fontScale,
-                ),
-                LocalConfiguration provides configuration,
-                // Needed so that UI that uses it don't crash during screenshot tests
-                LocalOnBackPressedDispatcherOwner provides object : OnBackPressedDispatcherOwner {
-                    override val onBackPressedDispatcher: OnBackPressedDispatcher =
-                        OnBackPressedDispatcher()
+@RunWith(TestParameterInjector::class)
+class ScreenTests {
 
-                    override val lifecycle: Lifecycle = lifecycleOwner.lifecycle
-                },
-            ) {
-                Box {
-                    componentPreview.content()
-                }
+    object ScreenPreviewProvider : TestParameter.TestParameterValuesProvider {
+        override fun provideValues(): List<ComponentPreview> =
+            Showkase.getMetadata().componentList.filter {
+                it.group == "Screens"
+            }.map(::ComponentPreview)
+    }
+
+    @get:Rule
+    val paparazziScreen = Paparazzi(
+        maxPercentDifference = 0.0,
+        showSystemUi = false,
+        renderingMode = SessionParams.RenderingMode.NORMAL,
+    )
+
+    @Test
+    fun screen_tests(
+        @TestParameter(valuesProvider = ScreenPreviewProvider::class) screenPreview: ComponentPreview,
+        @TestParameter(value = ["1", "2"]) fontScale: Float,
+        @TestParameter(value = ["light", "dark"]) theme: String,
+    ) {
+        paparazziScreen.snapshot {
+            compositionProvider(fontScale = fontScale, theme = theme) {
+                screenPreview.content()
             }
+        }
+    }
+}
+
+@Composable
+private fun compositionProvider(
+    fontScale: Float,
+    theme: String,
+    content: @Composable () -> Unit,
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val configuration = if (theme == "dark") {
+        Configuration(LocalConfiguration.current).apply {
+            uiMode = Configuration.UI_MODE_NIGHT_YES
+        }
+    } else {
+        LocalConfiguration.current
+    }
+
+    CompositionLocalProvider(
+        LocalInspectionMode provides true,
+        LocalDensity provides Density(
+            density = LocalDensity.current.density,
+            fontScale = fontScale,
+        ),
+        LocalConfiguration provides configuration,
+        // Needed so that UI that uses it don't crash during screenshot tests
+        LocalOnBackPressedDispatcherOwner provides object : OnBackPressedDispatcherOwner {
+            override val onBackPressedDispatcher: OnBackPressedDispatcher =
+                OnBackPressedDispatcher()
+
+            override val lifecycle: Lifecycle = lifecycleOwner.lifecycle
+        },
+    ) {
+        Box {
+            content.invoke()
         }
     }
 }
