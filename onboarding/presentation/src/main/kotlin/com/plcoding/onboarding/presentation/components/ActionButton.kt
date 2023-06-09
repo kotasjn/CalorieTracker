77d77743d762b97297be2304c6c319ebc47feab4
Jan Kotas <jan.kotas@eman.cz>
2023-06-09 13:01:46 +0200

    💄 Add action button preview and screenshots

--- onboarding/presentation/src/main/kotlin/com/plcoding/onboarding/presentation/components/ActionButton.kt
+++ onboarding/presentation/src/main/kotlin/com/plcoding/onboarding/presentation/components/ActionButton.kt
@@ -1,5 +1,8 @@
 package com.plcoding.onboarding.presentation.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -9,7 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import com.airbnb.android.showkase.annotation.ShowkaseComposable
+import com.plcoding.coreui.ComponentPreview
 import com.plcoding.coreui.LocalSpacing
+import com.plcoding.coreui.PreviewSurface
 
 @Composable
 fun ActionButton(
@@ -31,5 +37,26 @@ fun ActionButton(
             color = MaterialTheme.colors.onPrimary,
             modifier = Modifier.padding(LocalSpacing.current.spaceSmall),
         )
+    }
+}
+
+@ComponentPreview
+@ShowkaseComposable(name = "ActionButton", group = "Components")
+@Composable
+fun ActionButtonPreview() {
+    PreviewSurface {
+        Column {
+            ActionButton(
+                text = "ButtonText",
+                onClick = {},
+                isEnabled = true,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            ActionButton(
+                text = "ButtonText",
+                onClick = {},
+                isEnabled = false,
+            )
+        }
     }
 }
